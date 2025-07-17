e66dfeb5889e3b3bc2687d6dfb359396978204de
Javier Sanhueza <115113865+LearGrinV@users.noreply.github.com>
2025-07-17 16:39:24 -0400

    cambios en la creacion de vehiculoConductor

--- postgres/schemas/vehiculoconductor.py
+++ postgres/schemas/vehiculoconductor.py
@@ -4,11 +4,15 @@ from typing import Optional
 from ..schemas.conductor import ConductorSchema
 from ..schemas.vehiculo import VehiculoSchema
 
-class VehiculoConductorSchema(BaseModel):
+class VehiculoConductorCreateSchema(BaseModel):
     id_vehiculo: int
+    id_conductor: int
     fecha_inicio: date
-    fecha_fin: Optional[date]
+    fecha_fin: Optional[date] = None
 
+class VehiculoConductorSchema(VehiculoConductorCreateSchema):
+    id_vehiculo: int
+    id_conductor: int
     conductor: ConductorSchema
     vehiculo : VehiculoSchema
 
